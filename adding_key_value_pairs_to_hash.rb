86587fdd0f4475b095d9b95e366d46e4ed2a7b91
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2019-12-11 22:19:16 +0000

    Adding key/value pairs to Kardashian kids hash

---
+++ adding_key_value_pairs_to_hash.rb
@@ -0,0 +1,10 @@
+kardashian_kids = {}
+kardashian_kids.merge!(kim: ["north", "saint", "chicago", "psalm"]).merge!
+(kourtney: ["mason", "penelope", "reign"]).merge!(khloe: "true")
+.merge!(rob: "dream").merge!(kylie: "stormi")
+
+#=> => {:kim=>["north", "saint", "chicago", "psalm"], 
+:kourtney=>["mason", "penelope", "reign"], 
+:khloe=>"true", 
+:rob=>"dream", 
+:kylie=>"stormi"}
