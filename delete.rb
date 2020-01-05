6d1cd655b7790a6763999fe41919bce4f8458e82
lajoiedevivre89 <emilyspencer89@hotmail.co.uk>
2020-01-05 00:04:35 +0000

    Adds output and another hash with different name due to destructive nature of delete

--- delete.rb
+++ delete.rb
@@ -1,11 +1,21 @@
 # Delete
 
-my_hash = { a: "peony", b: "tulip", c: "rose", d: 12, e: ["arrays", "are", "cool"] }
+# Delete is destructive - that is to say that it modifies the original hash
+# so myhash2 - the second time I use delete has already been modified and had is (d: 12) deleted
 
+my_hash = { a: "peony", b: "tulip", c: "rose", d: 12, e: ["arrays", "are", "cool"] }
+my_hash2 = { a: "peony", b: "tulip", c: "rose", d: 12, e: ["arrays", "are", "cool"] }
 
 puts my_hash.each { |key, value|
   my_hash.delete(key) if value.is_a? String }
 
-puts my_hash.each { |key, value|
-  my_hash.delete(key) if value.is_a? Integer }
+  #=> {:d=>12, :e=>["arrays", "are", "cool"]}
 
+puts my_hash2.each { |key, value|
+  my_hash2.delete(key) if value.is_a? Integer }
+
+  #=> {:a=>"peony", :b=>"tulip", :c=>"rose", :e=>["arrays", "are", "cool"]}
+
+puts my_hash2.each { |key, value| my_hash2.delete(key) if value.is_a? Array }
+
+#=> {:a=>"peony", :b=>"tulip", :c=>"rose"}
